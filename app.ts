53baa75ed3e3b48cf91f0b706ef69740943cd9d4
Aditya Choudhury <adityasubham03@gmail.com>
2024-01-05 11:31:06 +0530

    test

--- app.ts
+++ app.ts
@@ -1,64 +1,66 @@
-import express, { Request, Response, NextFunction } from "express";
-import cors from "cors";
-import { startServer } from "./Utils";
-import Auth from "./Routes/Auth";
-import cookieParser from "cookie-parser";
+import express, { Request, Response, NextFunction } from 'express'
+import cors from 'cors'
+import { startServer } from './Utils'
+import Auth from './Routes/Auth'
+import cookieParser from 'cookie-parser'
 
 //Server Initialization
-const app = express();
+const app = express()
 
 //Server Configurations
 app.use(
-	cors({
-		credentials: true,
-		origin: ["http://localhost:5173"],
-	})
-);
+  cors({
+    credentials: true,
+    origin: ['http://localhost:5173'],
+  }),
+)
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 //API routes starts here
 
 //Default routes
 app.use((req: Request, res: Response, next: NextFunction) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With, Content-Type, Accept");
-	next();
-});
-
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-	res.send({
-		data: {
-			appName: "Starter Pack | Backend",
-			developedBy: "Aditya Choudhury",
-			maintainedBy: "Aditya Choudhury",
-			version: "1.0.0.0",
-		},
-		success: true,
-	});
-});
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With, Content-Type, Accept',
+  )
+  next()
+})
+app.get('/', (req: Request, res: Response) => {
+  res.send({
+    data: {
+      appName: 'Starter Pack | Backend',
+      developedBy: 'Aditya Choudhury',
+      maintainedBy: 'Aditya Choudhury',
+      version: '1.0.0.0',
+    },
+    success: true,
+  })
+})
 
 //Health check API
-app.get("/health", (req: Request, res: Response) => {
-	return res.status(200).json({
-		status: 200,
-		message: "Server is up and running"
-	})
-});
+app.get('/health', (req: Request, res: Response) => {
+  return res.status(200).json({
+    status: 200,
+    message: 'Server is up and running',
+  })
+})
 
 //App Routes
-app.use("/api/auth", Auth);
+app.use('/api/auth', Auth)
 
 // Default not-found route
-app.use((req: Request, res: Response, next: NextFunction) => {
-	res.send({
-		reason: "invalid-request",
-		message:
-			"The endpoint you wanna reach is not available! Please check the endpoint again",
-		success: false,
-	});
-});
+app.use((req: Request, res: Response) => {
+  res.send({
+    reason: 'invalid-request',
+    message:
+      'The endpoint you wanna reach is not available! Please check the endpoint again',
+    success: false,
+  })
+})
 
-startServer(app);
+startServer(app)
