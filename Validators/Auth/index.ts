edbc51762be30cd5a2e8befd40c13b8b794f91e4
Aditya Choudhury <adityasubham03@gmail.com>
2024-05-07 13:00:20 +0530

    Updates to packages and controllers

--- Validators/Auth/index.ts
+++ Validators/Auth/index.ts
@@ -1,96 +1,105 @@
-import { NextFunction, Request, Response } from "express";
-import Joi from "joi";
-import Users from "../../Models/Users";
+import { NextFunction, Request, Response } from 'express'
+import Joi from 'joi'
+import Users from '../../Models/Users'
 
 interface customRequest extends Request {
-	user_id: string;
-	_id: string;
-	username: String;
-	token: String;
-	email: String;
-	role: String;
-	verified: Boolean;
+  user_id: string
+  _id: string
+  username: string
+  token: string
+  email: string
+  role: string
+  verified: boolean
 }
 
 const signupSchema = Joi.object({
-	name: Joi.string().required().min(1),
-	email: Joi.string().required(),
-	username: Joi.string().required(),
-	password: Joi.string()
-		.pattern(new RegExp("^[a-zA-Z0-9!@#$%^&*()_]{3,30}$"))
-		.min(8)
-		.required(),
-	mobile: Joi.number().min(10).required(),
-});
+  name: Joi.string().required().min(1),
+  email: Joi.string().required(),
+  username: Joi.string().required(),
+  password: Joi.string()
+    .pattern(new RegExp('^[a-zA-Z0-9!@#$%^&*()_]{3,30}$'))
+    .min(8)
+    .required(),
+  mobile: Joi.number().min(10).required(),
+})
 
 const loginSchema = Joi.object({
-	// name: Joi.string().required().min(1),
-	// email: Joi.string().email().required(),
-	username: Joi.string().required(),
-	password: Joi.string()
-		.pattern(new RegExp("^[a-zA-Z0-9!@#$%^&*()_]{3,30}$"))
-		.min(8)
-		.required(),
-});
+  // name: Joi.string().required().min(1),
+  // email: Joi.string().email().required(),
+  username: Joi.string().required(),
+  password: Joi.string()
+    .pattern(new RegExp('^[a-zA-Z0-9!@#$%^&*()_]{3,30}$'))
+    .min(8)
+    .required(),
+})
 
 const signupValidator = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) => {
-	try {
-		const signupRequest = await signupSchema.validateAsync(req.body);
-		next();
-	} catch (err: any) {
-		return res.status(err.status || 403).json({
-			message: err.message || "An error occurred",
-			success: false,
-		});
-	}
-};
+  try {
+    const signupRequest = await signupSchema.validateAsync(req.body)
+    next()
+  } catch (err: any) {
+    return res.status(err.status || 403).json({
+      message: err.message || 'An error occurred',
+      success: false,
+    })
+  }
+}
 
 const validateEmail = async (email: string) => {
-	const user = await Users.findOne({ email: email }).select("email");
-	return user ? true : false;
-};
+  const user = await Users.findOne({ email: email }).select('email')
+  return user ? true : false
+}
 
-const validateUsername = async (username: String) => {
-	const user = await Users.findOne({ username: username }).select("username");
-	return user ? true : false;
-};
+const validateUsername = async (username: string) => {
+  const user = await Users.findOne({ username: username }).select('username')
+  return user ? true : false
+}
 
 const verification = async (
-	req: customRequest,
-	res: Response,
-	next: NextFunction
+  req: customRequest,
+  res: Response,
+  next: NextFunction,
 ) => {
-	if (req.verified) {
-		return res.status(200).json({
-			reason: "verified",
-			message: "user already verified",
-			success: false,
-		});
-	} else {
-		next();
-	}
-};
+  if (req.verified) {
+    return res.status(200).json({
+      reason: 'verified',
+      message: 'user already verified',
+      success: false,
+    })
+  } else {
+    next()
+  }
+}
 const isOTP = (req: customRequest, res: Response, next: NextFunction) => {
-	if (req.body.otp) {
-		next();
-	} else {
-		return res.status(200).json({
-			reason: "no-OTP",
-			message: "no OTP was provided! Enter otp and try again!",
-			success: false,
-		});
-	}
-};
+  if (req.body.otp) {
+    next()
+  } else {
+    return res.status(200).json({
+      reason: 'no-OTP',
+      message: 'no OTP was provided! Enter otp and try again!',
+      success: false,
+    })
+  }
+}
+
+const passwordSchema = Joi.object({
+  otp: Joi.string().required(),
+  password: Joi.string()
+    .pattern(new RegExp('^[a-zA-Z0-9!@#$%^&*()_]{3,30}$'))
+    .min(8)
+    .required(),
+})
 
 export default {
-	signupValidator,
-	validateEmail,
-	validateUsername,
-	loginSchema,
-	verification,
-	isOTP,
-};
+  signupValidator,
+  validateEmail,
+  validateUsername,
+  loginSchema,
+  verification,
+  isOTP,
+  passwordSchema,
+}
